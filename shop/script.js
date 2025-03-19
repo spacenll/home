d7f5be00ed97e19283a362779e5f7d42673b443e
ALA ALDIN MOHAMED KHALIL <aaa5khalil@gmail.com>
2025-03-19 04:40:21 +0400

    Update script.js

--- shop/script.js
+++ shop/script.js
@@ -331,7 +331,20 @@ function sendWhatsApp() {
             const whatsappUrl = `https://wa.me/${phonew}?text=${encodeURIComponent(message)}`;
             const doneurl= '#ok';
             window.open(whatsappUrl, '_blank'); // فتح الرابط في نافذة جديدة
-               window.location.href = doneurl; 
+            
+            // Google tag (gtag.js)
+(function() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-975991783";
+    document.head.appendChild(script);
+
+    script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'AW-975991783');
+    };
         }
     });
 }
