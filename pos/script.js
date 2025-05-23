c88771d0087445d8df846db72de0dd6c4ee195a8
ALA ALDIN MOHAMED KHALIL <aaa5khalil@gmail.com>
2025-05-23 14:39:55 +0400

    Add files via upload

---
+++ pos/script.js
@@ -0,0 +1,21 @@
+// أنيميشن عند التمرير
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const options = {
+    threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    section.classList.add("fade-in");
+    observer.observe(section);
+  });
+});
